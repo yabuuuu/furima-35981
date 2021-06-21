6e700f432727cc47aa71ef2d8247b8ab072888d4
yabuuuu <475sunflower@gmail.com>
2021-06-21 23:45:10 +0900

    販売価格バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,9 +12,7 @@ class Item < ApplicationRecord
   
   validates :name, :description, :image, presence: true
   validates :category_id, :condition_id, :shipping_area_id, :shipping_burden_id, :shipping_date_id, numericality: { other_than: 1 } 
-  validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { in: 300..9999999 }
+  validates :price, format: { with: /\A[0-9]+\z/ }, presence: true
+  validates_inclusion_of :price, in:300..9999999
 
-  def tax_price
-    puts {price.to_i * 0.1}
-  end
 end
