881cdf05ce53ff507248f8ea17361f6bf034cc30
yabuuuu <475sunflower@gmail.com>
2021-06-22 11:40:44 +0900

    商品出品機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,10 +26,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name,:description,:category_id,:condition_id,:shipping_burden_id,:shipping_area_id,:shipping_date_id,:price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in? 
-      redirect_to new_user_session_path
-    end
-  end
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,27 +25,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include"Description can't be blank"
       end
       it 'カテゴリーが---では出品できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include"Category must be other than 1"
       end
       it '商品状態が---では出品できない' do
-        @item.condition_id = '1'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include"Condition must be other than 1"
       end
       it '送料負担が---では出品できない' do
-        @item.shipping_burden_id = '1'
+        @item.shipping_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include"Shipping burden must be other than 1"
       end
       it '発送元の地域が---では出品できない' do
-        @item.shipping_area_id = '1'
+        @item.shipping_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include"Shipping area must be other than 1"
       end
       it '発送までの日数の情報が---では出品できない' do
-        @item.shipping_date_id = '1'
+        @item.shipping_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include"Shipping date must be other than 1"
       end
@@ -55,17 +55,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include"Price can't be blank"
       end
       it '価格が300未満では出品できない' do
-        @item.price = '299'
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include"Price is not included in the list"
       end
       it '価格が9,999,999より大きいと出品できない' do
-        @item.price = '10000000'
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include"Price is not included in the list"
       end
       it '価格が全角だと出品できない' do
         @item.price = '１８００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include"Price is not included in the list"
+      end
+      it '価格が半角英数字混合では出品できない' do
+        @item.price = 'a1800'
+        @item.valid?
+        expect(@item.errors.full_messages).to include"Price is not included in the list"
+      end
+      it '価格が半角英字のみでは出品できない' do
+        @item.price = 'aaaaa'
         @item.valid?
         expect(@item.errors.full_messages).to include"Price is not included in the list"
       end
