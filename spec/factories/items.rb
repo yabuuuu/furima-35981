87c55b7009d9df0dad21b588b5463a3cebb10437
yabuuuu <475sunflower@gmail.com>
2021-06-22 14:27:50 +0900

    テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -10,6 +10,7 @@ FactoryBot.define do
     price                   {18000}
     association :user 
 
+    
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
     end
