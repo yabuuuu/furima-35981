9cf74550f8e8901f53c7d9f40ee0f6c3bdf3223f
yabuuuu <475sunflower@gmail.com>
2021-06-22 19:49:09 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
-
   with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/ } do
     validates :kana_sei
     validates :kana_mei
@@ -15,9 +13,9 @@ class User < ApplicationRecord
     validates :sei
     validates :mei
   end
-  
-  validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-z\d]+\z/i }
-  
+
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-z\d]+\z/i }
+
   validates :nickname, presence: true
-  validates :birthday, presence: true  
+  validates :birthday, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    #@user = FactoryBot.build(:user)
+    # @user = FactoryBot.build(:user)
     @item = FactoryBot.build(:item)
   end
 
@@ -17,67 +17,67 @@ RSpec.describe Item, type: :model do
       it '画像がないと出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include"Image can't be blank"
+        expect(@item.errors.full_messages).to include "Image can't be blank"
       end
       it '商品説明がないと出品できない' do
         @item.description = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include"Description can't be blank"
+        expect(@item.errors.full_messages).to include "Description can't be blank"
       end
       it 'カテゴリーが---では出品できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include"Category must be other than 1"
+        expect(@item.errors.full_messages).to include 'Category must be other than 1'
       end
       it '商品状態が---では出品できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include"Condition must be other than 1"
+        expect(@item.errors.full_messages).to include 'Condition must be other than 1'
       end
       it '送料負担が---では出品できない' do
         @item.shipping_burden_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include"Shipping burden must be other than 1"
+        expect(@item.errors.full_messages).to include 'Shipping burden must be other than 1'
       end
       it '発送元の地域が---では出品できない' do
         @item.shipping_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include"Shipping area must be other than 1"
+        expect(@item.errors.full_messages).to include 'Shipping area must be other than 1'
       end
       it '発送までの日数の情報が---では出品できない' do
         @item.shipping_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include"Shipping date must be other than 1"
+        expect(@item.errors.full_messages).to include 'Shipping date must be other than 1'
       end
       it '価格がないと出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price can't be blank"
+        expect(@item.errors.full_messages).to include "Price can't be blank"
       end
       it '価格が300未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price must be greater than or equal to 300"
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
       end
       it '価格が9,999,999より大きいと出品できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price must be less than or equal to 9999999"
+        expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
       end
       it '価格が全角だと出品できない' do
         @item.price = '１８００'
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
       it '価格が半角英数字混合では出品できない' do
         @item.price = 'a1800'
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
       it '価格が半角英字のみでは出品できない' do
         @item.price = 'aaaaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include"Price is not a number"
+        expect(@item.errors.full_messages).to include 'Price is not a number'
       end
     end
   end
