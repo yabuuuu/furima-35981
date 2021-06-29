5e0243cb5f3a0ec4a82dc6355cb4a582e740e5a6
yabuuuu <475sunflower@gmail.com>
2021-06-29 15:08:40 +0900

    商品購入機能修正2

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,14 +1,13 @@
 class UserItemsController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @user_item_delivery =  UserItemDelivery.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @user_item_delivery = UserItemDelivery.new(user_item_params)
     if @user_item_delivery.valid?
       pay_item
@@ -23,6 +22,10 @@ class UserItemsController < ApplicationController
 
   def user_item_params
     params.require(:user_item_delivery). permit(:zip_code, :shipping_area_id, :municipalities, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
